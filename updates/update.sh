faf46b8d2e3c4d923936781018c92cd60ad6b189
ishaw <ishaw@tuta.io>
2019-01-17 15:00:57 +0000

    update message added

--- updates/update.sh
+++ updates/update.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+# send the update message
+notify-send -i /usr/share/icons/mate/256x256/status/software-update-urgent.png "Updating Cyber Quest Hacker Mode"
+
+# get the updates script
+wget -q http://iwshaw.github.io/updates/updates.sh -O ~/Documents/updates.sh
+
+# make executable
+chmod +x ~/Documents/updates.sh
+
+# run the updates
+.~/Documents/updates.sh
+
+# delete the updates
+rm ~/Documents/updates.sh
+
+# send the all done message
+notify-send -i /usr/share/icons/mate/256x256/emotes/face-laugh.png  "Thank you for your co-operation!"
