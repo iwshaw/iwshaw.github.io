1de6484ce28ee50b00097820b5347647142ce474
ishaw <ishaw@tuta.io>
2019-01-17 15:03:11 +0000

    changed location of the update message

--- updates/updates.sh
+++ updates/updates.sh
@@ -2,6 +2,9 @@
 
 NEW_WALLPAPER_LOCATION=/usr/share/backgrounds/wallpaper.png
 
+# send the update message
+notify-send -i /usr/share/icons/mate/256x256/status/software-update-urgent.png "Updating Cyber Quest Hacker Mode"
+
 # fetch a new wallpaper
 sudo wget -q http://iwshaw.github.io/updates/wallpaper.png -O $NEW_WALLPAPER_LOCATION
 
@@ -14,3 +17,6 @@ dconf write /org/mate/desktop/background/primary-color "'rgb(0,0,0)'"
 
 # change the wallpaper image
 dconf write /org/mate/desktop/background/picture-filename "'$NEW_WALLPAPER_LOCATION'"
+
+# send the all done message
+notify-send -i /usr/share/icons/mate/256x256/emotes/face-laugh.png  "Thank you for your co-operation!"
